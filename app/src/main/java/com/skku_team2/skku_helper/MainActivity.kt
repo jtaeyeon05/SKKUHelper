cb3b2c0ff3203b2409dbb0f449f9c4cad1785936
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-10-29 17:18:04 +0900

    Feature: Add MainActivity

---
+++ app/src/main/java/com/skku_team2/skku_helper/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.skku_team2.skku_helper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.skku_team2.skku_helper.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            enableEdgeToEdge()
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
