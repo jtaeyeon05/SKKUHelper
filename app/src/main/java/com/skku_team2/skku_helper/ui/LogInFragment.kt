d62c753d08f9fd22d9a2a4b90f0bf0cc24e60ede
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-10-30 03:15:43 +0900

    Design: 로그인 화면 UI

--- app/src/main/java/com/skku_team2/skku_helper/ui/LogInFragment.kt
+++ app/src/main/java/com/skku_team2/skku_helper/ui/LogInFragment.kt
@@ -1,31 +1,72 @@
 package com.skku_team2.skku_helper.ui
 
-import androidx.fragment.app.viewModels
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.skku_team2.skku_helper.R
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.skku_team2.skku_helper.MainActivity
+import com.skku_team2.skku_helper.databinding.FragmentLoginBinding
+
 
 class LogInFragment : Fragment() {
-
-    companion object Companion {
-        fun newInstance() = LogInFragment()
-    }
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: LogInViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.textInputLayoutToken.editText?.addTextChangedListener {
+            viewModel.onTokenChanged(it?.toString().orEmpty())
+        }
+
+        binding.textInputLayoutToken.setOnClickListener {
+            // TODO: Dialog
+        }
+
+        binding.buttonConfirm.setOnClickListener {
+            // TODO: Log In
+            val mainActivityIntent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(mainActivityIntent)
+        }
+
+        viewModel.uiState.observe(viewLifecycleOwner) { state ->
+            val theme = context?.theme
+            binding.buttonConfirm.isEnabled = state.buttonLogInEnabled
+            if (state.buttonLogInEnabled) {
+                val colorButtonContainer = TypedValue()
+                val colorButtonOnContainer = TypedValue()
+                theme?.resolveAttribute(com.google.android.material.R.attr.colorPrimaryContainer, colorButtonContainer, true)
+                theme?.resolveAttribute(com.google.android.material.R.attr.colorOnPrimaryContainer, colorButtonOnContainer, true)
+                binding.buttonConfirm.setBackgroundColor(colorButtonContainer.data)
+                binding.buttonConfirm.setTextColor(colorButtonOnContainer.data)
+            } else {
+                val colorButtonContainer = TypedValue()
+                val colorButtonOnContainer = TypedValue()
+                theme?.resolveAttribute(com.google.android.material.R.attr.colorSurfaceContainerHighest, colorButtonContainer, true)
+                theme?.resolveAttribute(com.google.android.material.R.attr.colorOnSurfaceVariant, colorButtonOnContainer, true)
+                binding.buttonConfirm.setBackgroundColor(colorButtonContainer.data)
+                binding.buttonConfirm.setTextColor(colorButtonOnContainer.data)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/skku_team2/skku_helper/ui/LogInViewModel.kt
+++ app/src/main/java/com/skku_team2/skku_helper/ui/LogInViewModel.kt
@@ -1,7 +1,24 @@
 package com.skku_team2.skku_helper.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+
+data class LogInUiState(
+    val token: String = "",
+    val buttonLogInEnabled: Boolean = false,
+    // TODO: Loading, Error
+)
+
 class LogInViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _uiState = MutableLiveData(LogInUiState())
+    val uiState: LiveData<LogInUiState> = _uiState
+
+    fun onTokenChanged(text: String) {
+        _uiState.value = _uiState.value?.copy(
+            token = text,
+            buttonLogInEnabled = text.isNotBlank()
+        )
+    }
 }
