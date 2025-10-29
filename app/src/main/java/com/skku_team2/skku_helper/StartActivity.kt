c020f5640920586a1941b79140c1e40d2b34c8e8
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-10-29 16:58:38 +0900

    Rename: MainActivity -> StartActivity

--- app/src/main/java/com/skku_team2/skku_helper/StartActivity.kt
+++ app/src/main/java/com/skku_team2/skku_helper/StartActivity.kt
@@ -5,19 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.createGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.fragment
-import com.skku_team2.skku_helper.databinding.ActivityMainBinding
+import com.skku_team2.skku_helper.databinding.ActivityStartBinding
 import com.skku_team2.skku_helper.navigation.MainScreen
 import com.skku_team2.skku_helper.ui.HomeFragment
 import com.skku_team2.skku_helper.ui.LogInFragment
 
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
