97d9d3e149223027ba60bf619ceba1c640ec3cd1
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-12-03 13:02:40 +0900

    Merge: home_fragment_update

--- app/src/main/java/com/skku_team2/skku_helper/ui/main/HomeViewModel.kt
+++ app/src/main/java/com/skku_team2/skku_helper/ui/main/HomeViewModel.kt
@@ -9,9 +9,11 @@ import com.skku_team2.skku_helper.canvas.CombinedAssignmentInfo
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = CanvasRepository(application.applicationContext)
+
     val homepageData: LiveData<List<CombinedAssignmentInfo>> = repository.combinedData
     val isLoading: LiveData<Boolean> = repository.isLoading
     val error: LiveData<String> = repository.error
+
     fun loadData() {
         // 7. token 없이 호출 (Repository가 알아서 가져옴)
         repository.fetchHomepageData()
